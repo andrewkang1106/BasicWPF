5ef310fa85f909b479b90f4d27bab78fd963774c
Andrew Kang <andrewkang1106@gmail.com>
2022-06-27 15:44:00 -0400

    ServerWPF: created folders Models, Views, ViewModels. Edited path for MainWindow -> Views/MainWindow

--- ServerWPF/Views/MainWindow.xaml.cs
+++ ServerWPF/Views/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ServerWPF
+namespace ServerWPF.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
